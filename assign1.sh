da5c635145fe46e16a6dd194493a36060e908aa0
manishac27 <138668130+manishac27@users.noreply.github.com>
2023-07-05 16:55:31 +0530

    Create assign1.sh

---
+++ assign1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash  
+
+# Read the user input   
+
+echo "Enter the user name: "  
+read first_name
+echo " $first_name"  
+
+
+~                                                                                                                                                     
+~                                                                                                                                                     
+~                                                                                                                                                     
+~                                                                                                                                                     
+~                 
